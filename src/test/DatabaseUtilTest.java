9679e3e30df65fe9de78f3f6e4502426e4f799e7
b05902121 <b05902121@ntu.edu.tw>
2019-12-28 16:32:23 +0800

    fix bugs and comments

--- src/test/DatabaseUtilTest.java
+++ src/test/DatabaseUtilTest.java
@@ -9,7 +9,7 @@ import databaseUtil.DatabaseUser;
 public class DatabaseUtilTest{
     public static void main(String[] args) {
         try {
-            /*  Initial databases and tables */
+            /*  Initialize databases and tables */
             DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("jdbc.properties");
             if(!databaseBuildAllTables.getConnection().isClosed()) {
                 System.out.print("[LOG] databaseBuildAllTables start.\n");
@@ -17,7 +17,7 @@ public class DatabaseUtilTest{
             databaseBuildAllTables.start();
             databaseBuildAllTables.closeConnection();
 
-            /*  Set default user data to users table and select one data    */
+            /*  Set default user data to users' table and select one data */
             DatabaseUser databaseUser = new DatabaseUser("jdbc.properties");
             if(!databaseUser.getConnection().isClosed()) {
                 System.out.print("[LOG] DatabaseUser start.\n");
@@ -28,7 +28,7 @@ public class DatabaseUtilTest{
             System.out.print("[LOG] ---- Result start ----\n");
             while (resultSet.next()){
                 System.out.print(resultSet.getString("UID") + " - "
-                                + resultSet.getString("UID") + '\n');
+                                + resultSet.getString("password") + '\n');
             }
             System.out.print("[LOG] ---- Result end ----\n");
             databaseUser.closeConnection();
