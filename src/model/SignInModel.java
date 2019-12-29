27045b4fa5120b7c3c7e4e2ff866dc3dbb9e5cf1
b05902121 <b05902121@ntu.edu.tw>
2019-12-29 23:53:04 +0800

    SignInModel.java

---
+++ src/model/SignInModel.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.sql.*;
+import databaseUtil.DatabaseUser;
+
+public class SignInModel {
+    
+    private DatabaseUser dbUser = null;
+
+    public SignInModel() {
+        this.dbUser = new DatabaseUser("config/jdbc.properties");
+    }
+
+    public Boolean signIn(String username, String pwd) throws SQLException {
+        /*
+         * First gets user by username.
+         * If username does not exist, return false.
+         * If username exists, check whether password is correct.
+         * If password is correct, return true. Else, return false.
+         */
+
+        ResultSet resultSet = dbUser.getUserById(username);
+
+        if(resultSet.next() == Boolean.FALSE) {
+            // username does not exists
+            return Boolean.FALSE;
+        }
+        else if (resultSet.getString("password").compareTo(pwd) == 0) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+}
