84942918a93c668eaf23df73e7f395ce3e7dfd0f
michael0703 <liaoweiskewer0703@gmail.com>
2019-12-30 20:19:33 +0800

    modify menu view button and create

---
+++ src/view/QueryView.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import view.BaseView;
+public class QueryView extends BaseView{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QueryView window = new QueryView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public QueryView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
