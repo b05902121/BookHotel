18e27ab64358d3207af42b64c68a8ff46c210b76
b05902109 <b05902109@ntu.edu.tw>
2019-12-31 13:25:28 +0800

    add buildAllTables in Main, default is forceDropTables

--- src/databaseUtil/DatabaseBuildAllTables.java
+++ src/databaseUtil/DatabaseBuildAllTables.java
@@ -1,50 +1,56 @@
 package databaseUtil;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Connection;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
+import java.lang.reflect.Type;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DatabaseBuildAllTables extends DatabaseConnect{
+    private StringBuilder stringBuilder = new StringBuilder();
+
     public DatabaseBuildAllTables(String configFile) {
         super(configFile);
     }
 
-    public void start(){
+    public void start(Boolean forceDropTable){
         System.out.print("[dbUtil] DatabaseBuildAllTables start().\n");
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("config/buildAllTables.sql"));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null){
-                if(!line.equals("")){
-                    stringBuilder.append(line);
+        if (forceDropTable) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader("config/buildAllTables.sql"));
+                stringBuilder.setLength(0);
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    if (!line.equals("")) {
+                        stringBuilder.append(line);
+                    }
                 }
-            }
-            reader.close();
-            String[] commands = stringBuilder.toString().split(";");
-            for(int i = 0; i < commands.length; i++){
-                if(!commands[i].equals("")){
-                    stmt.executeUpdate(commands[i]);
+                reader.close();
+                String[] commands = stringBuilder.toString().split(";");
+                for (int i = 0; i < commands.length; i++) {
+                    if (!commands[i].equals("")) {
+                        stmt.executeUpdate(commands[i]);
+                    }
                 }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
-    public void insertDefaultData(){
+    public void insertDefaultData(String defaultDataFilePath){
         System.out.print("[dbUtil] DatabaseUser insertDefaultData().\n");
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("config/InsertDefaultData.sql"));
-            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(defaultDataFilePath));
+            stringBuilder.setLength(0);
             String line = null;
             while ((line = reader.readLine()) != null){
                 if(!line.equals("")){
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,20 @@
 package main;
+import java.sql.SQLException;
+
 import controller.RoutingController;
+import databaseUtil.DatabaseBuildAllTables;
 
 public class Main {
     public static void main(String[] args) {
+    DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("config/jdbc.properties");
+    Boolean forceDropTable = true;
+    databaseBuildAllTables.start(forceDropTable);
+    databaseBuildAllTables.insertDefaultData("config/InsertDefaultData.sql");
+    try {
+		databaseBuildAllTables.closeConnection();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
         RoutingController router = new RoutingController();
         router.start();
     }
