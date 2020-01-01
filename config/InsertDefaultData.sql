48e3221ce924a4fed148b2b6beb1e9a8af28c093
b05902109 <b05902109@ntu.edu.tw>
2020-01-01 16:13:07 +0800

    fix G.B. asking: main, databaseUtilUserTest

--- config/InsertDefaultData.sql
+++ config/InsertDefaultData.sql
@@ -2,16 +2,3 @@ INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902002' , 'b05902002' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902059' , 'b05902059' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902109' , 'b05902109' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902129' , 'b05902129' );
-
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`) VALUES ('0','1','Taipei','East');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('0','0','SingleRoom','1');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('1','0','DoubleRoom','2');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('2','0','QuadRoom','3');
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`) VALUES ('1','1','Taipei','South');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('3','1','SingleRoom','1');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('4','1','DoubleRoom','2');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('5','1','QuadRoom','3');
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`) VALUES ('2','1','Taipei','West');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('6','2','SingleRoom','1');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('7','2','DoubleRoom','2');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`) VALUES ('8','2','QuadRoom','3');
--- src/test/DatabaseUtilUserTest.java
+++ src/test/DatabaseUtilUserTest.java
@@ -6,35 +6,45 @@ import databaseUtil.DatabaseBuildAllTables;
 import databaseUtil.DatabaseUser;
 
 public class DatabaseUtilUserTest {
-	public static void main(String[] args) {
-		try {
-			/*  Initialize databases and tables */
-			DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("config/jdbc.properties");
-			if (!databaseBuildAllTables.getConnection().isClosed()) {
-				System.out.print("[LOG] databaseBuildAllTables successfully start.\n");
-			}
-			databaseBuildAllTables.start();
-			databaseBuildAllTables.insertDefaultData();
-			databaseBuildAllTables.closeConnection();
+    public static void main(String[] args) {
+        try {
+            /*  Initial databases and tables */
+            DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("config/jdbc.properties");
+            if (!databaseBuildAllTables.getConnection().isClosed()) {
+                System.out.print("[LOG] databaseBuildAllTables successfully start.\n");
+            }
+            /* if you execute in the first time */
+            Boolean forceDropTable = true;
+            /* if you have been executed */
+//            Boolean forceDropTable = false;
 
-			ResultSet resultSet;
+            databaseBuildAllTables.start(forceDropTable);
+            
+            /* if you want to insert InsertDefaultData.sql (when test login/signin)*/
+            databaseBuildAllTables.insertDefaultData("config/InsertDefaultData.sql");
+            /* if you want to insert Json Data (when test book hotel) */
+//            databaseBuildAllTables.insertJsonData();
 
-			/*  Insert one user data and Select one data */
-			DatabaseUser databaseUser = new DatabaseUser("config/jdbc.properties");
-			if (!databaseUser.getConnection().isClosed()) {
-				System.out.print("[LOG] DatabaseUser successfully start.\n");
-			}
-			databaseUser.InsertUser("b05902000", "b05902000_12345");
-			resultSet = databaseUser.getUserById("b05902000");
-			System.out.print("[LOG] ---- Result start ----\n");
-			while (resultSet.next()) {
-				System.out.print("[LOG] " + resultSet.getString("UID") + "\t"
-						+ resultSet.getString("password") + '\n');
-			}
-			System.out.print("[LOG] ---- Result end ----\n");
-			databaseUser.closeConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+            databaseBuildAllTables.closeConnection();
+
+            ResultSet resultSet;
+
+            /*  Insert one user data and Select one data */
+            DatabaseUser databaseUser = new DatabaseUser("config/jdbc.properties");
+            if (!databaseUser.getConnection().isClosed()) {
+                System.out.print("[Test] DatabaseUser successfully start.\n");
+            }
+            databaseUser.InsertUser("b05902000", "b05902000");
+            resultSet = databaseUser.getUserById("b05902000");
+            System.out.print("[Test] ---- Result start ----\n");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString("UID") + "\t"
+                        + resultSet.getString("password") + '\n');
+            }
+            System.out.print("[Test] ---- Result end ----\n");
+            databaseUser.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
