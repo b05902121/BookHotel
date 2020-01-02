79e2831114b48dc1b5924c142a64b0f5ee3ed7bb
b05902121 <b05902121@ntu.edu.tw>
2020-01-02 14:45:21 +0800

    fix src/test/TestSignUpModel.java src/test/TestSignInModel.java

--- src/test/TestSignUpModel.java
+++ src/test/TestSignUpModel.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.sql.*;
+
+import controller.RoutingController;
 import model.SignUpModel;
 import databaseUtil.DatabaseBuildAllTables;
 import databaseUtil.DatabaseUser;
@@ -8,25 +10,23 @@ import databaseUtil.DatabaseUser;
 public class TestSignUpModel {
 
     public TestSignUpModel() throws SQLException {
-        /*  Initialize databases and tables */
         DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("config/jdbc.properties");
-        if(!databaseBuildAllTables.getConnection().isClosed()) {
-            System.out.print("[LOG] databaseBuildAllTables start.\n");
+        Boolean forceDropTable = true;
+        databaseBuildAllTables.start(forceDropTable);
+        databaseBuildAllTables.insertDefaultData("config/InsertDefaultData.sql");
+        try {
+            databaseBuildAllTables.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        databaseBuildAllTables.start();
-        databaseBuildAllTables.closeConnection();
-
-        /*  Set default user data to users table and select one data    */
-        DatabaseUser databaseUser = new DatabaseUser("config/jdbc.properties");
-        if(!databaseUser.getConnection().isClosed()) {
-            System.out.print("[LOG] DatabaseUser start.\n");
+            RoutingController router = new RoutingController();
+            router.start();
         }
-    }
 
     public static void main(String[] args) throws SQLException {
         SignUpModel signUpModel = new SignUpModel();
 
-        Boolean ret = signUpModel.signUp("gggg", "123123");
+        Boolean ret = signUpModel.signUp("fdfa", "123123");
         if (ret) {System.out.println("Sign up successfully.");}
         else {System.out.println("User already exists.");}
 
