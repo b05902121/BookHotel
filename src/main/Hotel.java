2246e00087a1a0fcc7610de6741f0f5a97f7cdd9
b05902109 <b05902109@ntu.edu.tw>
2020-01-02 15:25:46 +0800

    update main/hotel.java, fix address/locality order

--- src/main/Hotel.java
+++ src/main/Hotel.java
@@ -1,24 +1,22 @@
 package main;
 
-import java.util.ArrayList;
-
 public class Hotel {
     private Integer hotelId;
     private Integer hotelStar;
     private String locality;
     private String address;
-    private ArrayList<Room> singleRoom;
-    private ArrayList<Room> doubleRoom;
-    private ArrayList<Room> quadRoom;
+    private Integer singleRoomPrice;
+    private Integer doubleRoomPrice;
+    private Integer quadRoomPrice;
     
-    public Hotel(Integer hotelId, Integer hotelStar, String address, String locality, Integer snum, Integer dnum, Integer qnum) {
+    public Hotel(Integer hotelId, Integer hotelStar, String address, String locality, Integer sPrice, Integer dPrice, Integer qPrice) {
         this.hotelId = hotelId;
         this.hotelStar = hotelStar;
         this.address = address;
         this.locality = locality;
-        this.singleRoom = new ArrayList<Room>(snum);
-        this.doubleRoom = new ArrayList<Room>(dnum);
-        this.quadRoom = new ArrayList<Room>(qnum);
+        this.singleRoomPrice = sPrice;
+        this.doubleRoomPrice = dPrice;
+        this.quadRoomPrice = qPrice;
     }
 
     public Integer getHotelId() {
@@ -29,5 +27,24 @@ public class Hotel {
         return hotelStar;
     }
     
+    public Integer getSingleRoomPrice() {
+    	return singleRoomPrice;
+    }
+    
+    public Integer getDoubleRoomPrice() {
+    	return doubleRoomPrice;
+    }
+    
+    public Integer getQuadRoomPrice() {
+    	return quadRoomPrice;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
     
 }
