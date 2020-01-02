e7e69cf3d8d205374042f5bd16e11cb7d3cfa965
b05902121 <b05902121@ntu.edu.tw>
2020-01-02 15:17:50 +0800

    [change attr] remove 3 room arraylist, add price for 3 rooms

--- src/main/Hotel.java
+++ src/main/Hotel.java
@@ -1,24 +1,22 @@
 package main;
 
-import java.util.ArrayList;
-
 public class Hotel {
     private Integer hotelId;
     private Integer hotelStar;
     private String locality;
     private String address;
-    private ArrayList<Room> singleRoom;
-    private ArrayList<Room> doubleRoom;
-    private ArrayList<Room> quadRoom;
+    private Integer singleRoomPrice;
+    private Integer doubleRoomPrice;
+    private Integer quadRoomPrice;
     
-    public Hotel(Integer hotelId, Integer hotelStar, String address, String locality, Integer snum, Integer dnum, Integer qnum) {
+    public Hotel(Integer hotelId, Integer hotelStar, String address, String locality, Integer sPrice, Integer dPrice, Integer qPrice) {
         this.hotelId = hotelId;
         this.hotelStar = hotelStar;
         this.address = address;
         this.locality = locality;
-        this.singleRoom = new ArrayList<Room>(snum);
-        this.doubleRoom = new ArrayList<Room>(dnum);
-        this.quadRoom = new ArrayList<Room>(qnum);
+        this.singleRoomPrice = sPrice;
+        this.doubleRoomPrice = dPrice;
+        this.quadRoomPrice = qPrice;
     }
 
     public Integer getHotelId() {
@@ -30,26 +28,23 @@ public class Hotel {
     }
     
     public Integer getSingleRoomPrice() {
-    	return singleRoom.get(0).getRoomPrice();
+    	return singleRoomPrice;
     }
     
     public Integer getDoubleRoomPrice() {
-    	return doubleRoom.get(0).getRoomPrice();
+    	return doubleRoomPrice;
     }
     
     public Integer getQuadRoomPrice() {
-    	return quadRoom.get(0).getRoomPrice();
+    	return quadRoomPrice;
+    }
+
+    public String getLocality() {
+        return locality;
     }
     
-    public int getSingleRoomNum() {
-    	return singleRoom.size();
+    public String getAddress() {
+        return address;
     }
     
-    public int getDoubleRoomNum() {
-    	return doubleRoom.size();
-    }
-    
-    public int getQuadRoomNum() {
-    	return quadRoom.size();
-    }
 }
