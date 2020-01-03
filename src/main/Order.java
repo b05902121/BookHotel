58d14790d79fe632e3bb2350e9e51c136edf6ff6
b05902121 <b05902121@ntu.edu.tw>
2020-01-03 20:35:34 +0800

    order class

---
+++ src/main/Order.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Date;
+
+public class Order {
+
+    private Integer orderId;
+    private String username;
+    
+    private Integer hotelId;
+    private Date startDate;
+    private Date endDate;
+    private Integer sNum;
+    private Integer dNum;
+    private Integer qNum;
+    
+}
+
