9f2c0bb803d47da2895ecf6e9a608530fb6852d4
BaiiYuan <b05902002@ntu.edu.tw>
2020-01-04 14:26:02 +0800

    [Config] Fix Insert Data

--- config/InsertDefaultData.sql
+++ config/InsertDefaultData.sql
@@ -2,16 +2,4 @@ INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902002' , 'b05902002' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902059' , 'b05902059' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902109' , 'b05902109' );
 INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902129' , 'b05902129' );
-
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`,`SNum`,`DNum`,`QNum`,`SPrice`,`DPrice`,`QPrice`) VALUES ('0','1','Taipei','East','1','1','1','1','2','3'),;
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('0','0','SingleRoom','1','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('1','0','DoubleRoom','2','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('2','0','QuadRoom','3','0');
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`,`SNum`,`DNum`,`QNum`,`SPrice`,`DPrice`,`QPrice`) VALUES ('1','1','Taipei','South','1','1','1','1','2','3');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('3','1','SingleRoom','1','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('4','1','DoubleRoom','2','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('5','1','QuadRoom','3','0');
-INSERT INTO `Hotels`(`HotelID`,`HotelStar`,`Locality`,`Street-Address`,`SNum`,`DNum`,`QNum`,`SPrice`,`DPrice`,`QPrice`) VALUES ('2','1','Taipei','West','1','1','1','1','2','3');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('6','2','SingleRoom','1','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('7','2','DoubleRoom','2','0');
-INSERT INTO `Rooms`(`RoomID`,`HotelID`,`RoomType`,`RoomPrice`,`DateIsAvailable`) VALUES ('8','2','QuadRoom','3','0');
+INSERT INTO `Users`(`UID`,`password`) VALUES ( 'aaa' , 'aaa' );
