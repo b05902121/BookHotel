3aeca0ac3d7ecc261647107a2f8d2676f7e13f74
b05902109 <b05902109@ntu.edu.tw>
2020-01-04 13:26:02 +0800

    add insert default data

---
+++ config/InsertDefaultData.sql
@@ -0,0 +1,4 @@
+INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902002' , 'b05902002' );
+INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902059' , 'b05902059' );
+INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902109' , 'b05902109' );
+INSERT INTO `Users`(`UID`,`password`) VALUES ( 'b05902129' , 'b05902129' );
