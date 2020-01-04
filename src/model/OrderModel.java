8fcafc07d064e3a0e5f4e9ea80e2a1e1293179a6
b05902121 <b05902121@ntu.edu.tw>
2020-01-05 00:30:41 +0800

    [model/order] add reviseOrder and deleteOrder method

--- src/model/OrderModel.java
+++ src/model/OrderModel.java
@@ -44,4 +44,30 @@ public class OrderModel {
         ArrayList<Order> orderList = this.dbOrder.getOrderList(username);
         return orderList;
     }
+    
+    public Boolean reviseOrder(Integer orderId, String username, Integer hotelId, Date startDate, Date endDate, Integer sNum,
+            Integer dNum, Integer qNum, Integer totalPrice) throws ParseException {
+        Integer startDate_int = datetoint(startDate);
+        Integer endDate_int = datetoint(endDate);
+        Order oldOrder = null;
+        ArrayList<Order> orderList = findOrderByUsername(username);
+        for (Order o:orderList) {
+            if(o.getOrderId() == orderId) {oldOrder = o;}
+        }
+        if(startDate_int < oldOrder.getStartDate() || endDate_int > oldOrder.getEndDate()) {
+            System.out.println("[model/Order] Invalid start date and end date for revising order!");
+            return Boolean.FALSE;
+        }
+        Order newOrder = new Order(orderId, username, hotelId, startDate_int, endDate_int, sNum, dNum, qNum, totalPrice);
+        this.dbOrder.updateOrder(newOrder);
+        return Boolean.TRUE;
+    }
+    public void deleteOrder(Integer orderId, String username) {
+        Order order = null;
+        ArrayList<Order> orderList = findOrderByUsername(username);
+        for (Order o:orderList) {
+            if(o.getOrderId() == orderId) {order = o;}
+        }
+        this.dbOrder.deleteOrder(order);
+    }
 }
