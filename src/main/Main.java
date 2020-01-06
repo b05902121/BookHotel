a1e75794167a1e229f4771aa2b4810cfab2b6cba
b05902121 <b05902121@ntu.edu.tw>
2020-01-07 00:27:00 +0800

    [main] fix database always reload problem

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,32 @@
 package main;
+import java.io.IOException;
 import java.sql.SQLException;
 
 import controller.Router;
 import databaseUtil.DatabaseBuildAllTables;
+import main.ConfigPropertyValues;
 
 public class Main {
     public static void main(String[] args) {
         DatabaseBuildAllTables databaseBuildAllTables = new DatabaseBuildAllTables("config/jdbc.properties");
-        
-        /* if you execute in the first time */
-        Boolean forceDropTable = true;
-        /* if you have been executed */
-//        Boolean forceDropTable = false;
-
+        ConfigPropertyValues properties = new ConfigPropertyValues();
+        Boolean forceDropTable = null;
+        try {
+            forceDropTable = properties.getPropValues();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         databaseBuildAllTables.start(forceDropTable);
+        if(forceDropTable == Boolean.TRUE) {
+            /* if you want to insert InsertDefaultData.sql (when test login/signin)*/
+            databaseBuildAllTables.insertDefaultData("config/InsertDefaultData.sql");
+            /* if you want to insert Json Data (when test book hotel) */
+            databaseBuildAllTables.insertJsonData();
+            
+            // change forceDropTable of config.properties into FALSE
+            properties.updateConfigProperties("FALSE");
+        }
         
-        /* if you want to insert InsertDefaultData.sql (when test login/signin)*/
-        databaseBuildAllTables.insertDefaultData("config/InsertDefaultData.sql");
-        /* if you want to insert Json Data (when test book hotel) */
-        databaseBuildAllTables.insertJsonData();
-
         try {
             databaseBuildAllTables.closeConnection();
         } catch (SQLException e) {
